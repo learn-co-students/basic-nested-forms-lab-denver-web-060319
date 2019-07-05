f2c5a5462fbdad0c9cff223241c8dff4412bdc9f
github username <github email address>
2019-07-05 10:13:37 -0600

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,42 @@
 class RecipesController < ApplicationController
+  
+  before_action :find_recipe, only: [:show]
+
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def index
-    @recipes = Recipe.all
+    if params[:search]
+      @recipes = Recipe.where("title LIKE ?", "%#{params[:search]}")
+    else
+      @recipes = Recipe.all
+    end
   end
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.valid?
+      @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
 end
